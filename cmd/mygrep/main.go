eff71f7008c8776358249c12bc6f4d0a5f96ece3
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-09-21 18:10:08 +0530

    combine the character classes

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"os"
 	"strings"
-	"unicode/utf8"
 )
 
 func main() {
@@ -36,43 +34,87 @@ func main() {
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) == 0 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
-	}
-
 	var ok bool = false
-	var toMatch strings.Builder
-	if pattern == "\\d" {
-		for c := '0'; c <= '9'; c++ {
-			toMatch.WriteString(string(c))
+
+	lineLength := len(line)
+
+	for ind := 0; ind < lineLength; ind++ {
+		if matchChar(line, pattern, ind) {
+			ok = true
+			break
 		}
-	} else if pattern == "\\w" {
-		for c := '0'; c <= '9'; c++ {
-			toMatch.WriteString(string(c))
-		}
-		for c := 'a'; c <= 'z'; c++ {
-			toMatch.WriteString(string(c))
-		}
-		for c := 'A'; c <= 'Z'; c++ {
-			toMatch.WriteString(string(c))
-		}
-	} else if pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
-		if len(pattern) >= 3 && pattern[1] == '^' {
-			toCheck := pattern[2 : len(pattern)-1]
-			for _, c := range line {
-				if !strings.Contains(toCheck, string(c)) {
-					return true, nil
-				}
-			}
-			return false, nil
-		}
-		toMatch.WriteString(pattern[1 : len(pattern)-1])
-	} else {
-		toMatch.WriteString(pattern)
 	}
-
-	matchString := toMatch.String()
-	ok = bytes.ContainsAny(line, matchString)
-
 	return ok, nil
+}
+
+func matchDigit(char byte) bool {
+	return char >= '0' && char <= '9'
+}
+
+func matchAlphaNumeric(char byte) bool {
+	return matchDigit(char) || (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || char == '_'
+}
+
+func matchPositiveCharacterGroup(char byte, str string) bool {
+	for _, c := range str {
+		if c == rune(char) {
+			return true
+		}
+	}
+	return false
+}
+
+func matchNegativeCharacterGroup(char byte, str string) bool {
+	for _, c := range str {
+		if c == rune(char) {
+			return false
+		}
+	}
+	return true
+}
+
+func matchChar(line []byte, pattern string, ind int) bool {
+	if ind >= len(line) {
+		return false
+	}
+	char := line[ind]
+	patternIndex := 0
+	if len(pattern) >= 2 && pattern[:2] == "\\d" {
+		if !matchDigit(char) {
+			return false
+		}
+		patternIndex = 2
+	} else if len(pattern) >= 2 && pattern[:2] == "\\w" {
+		if !matchAlphaNumeric(char) {
+			return false
+		}
+		patternIndex = 2
+	} else if pattern[0] == '[' {
+		rightBracket := strings.Index(pattern, "]")
+		if rightBracket == -1 {
+			fmt.Fprintf(os.Stderr, "unsupported pattern\n")
+			os.Exit(2)
+		}
+		charGroup := pattern[1:rightBracket]
+		if charGroup[0] == '^' {
+			charGroup = charGroup[1:]
+			if !matchNegativeCharacterGroup(char, charGroup) {
+				return false
+			}
+		} else {
+			if !matchPositiveCharacterGroup(char, charGroup) {
+				return false
+			}
+		}
+		patternIndex = rightBracket + 1
+	} else {
+		if char != byte(pattern[0]) {
+			return false
+		}
+		patternIndex = 1
+	}
+	if patternIndex >= len(pattern) {
+		return true
+	}
+	return matchChar(line, pattern[patternIndex:], ind+1)
 }
